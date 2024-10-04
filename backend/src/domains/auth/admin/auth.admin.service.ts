68d5f09c2a399b22607a46400d37e460b9845846
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-10-04 09:28:04 +0700

    update: authenticate for admin

--- backend/src/domains/auth/admin/auth.admin.service.ts
+++ backend/src/domains/auth/admin/auth.admin.service.ts
@@ -1,4 +1,11 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import {
+	BadRequestException,
+	Injectable,
+	InternalServerErrorException,
+	Logger,
+	NotFoundException,
+	UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { verify } from '@/utils/hash.util';
 import { AdminsService } from '@/domains/admins/admins.service';
@@ -16,27 +23,40 @@ export class AuthAdminService {
 		username: string,
 		password: string,
 	): Promise<{ access_token: string }> {
-		const user = await this.adminService.getByUsername(username);
+		const user = await this.adminService.getByUsernameWithPassword(username);
+
+		if (!username || username.trim() === '') {
+			this.logger.error('Username must be a non-empty string.');
+			throw new BadRequestException('Username must be a non-empty string.');
+		}
 
 		if (!user) {
 			this.logger.error(`Admin with username ${username} not found!`);
-
 			throw new NotFoundException(`Admin with username ${username} not found!`);
+		}
+
+		if (!password || password.trim() === '') {
+			this.logger.error('Password must be a non-empty string.');
+			throw new BadRequestException('Password must be a non-empty string.');
+		}
+
+		this.logger.debug(`Hashed password from DB: ${user.hashedPassword}`);
+
+		if (!user.hashedPassword || user.hashedPassword.trim() === '') {
+			this.logger.error('Hashed password is invalid.');
+			throw new InternalServerErrorException('Hashed password is invalid.');
 		}
 
 		const verifyPassword = await verify(user.hashedPassword, password);
 
 		if (!verifyPassword) {
-			this.logger.error(
-				`Invalid password for admin with username ${username}!`,
-			);
-
-			throw new NotFoundException(
-				`Invalid password for admin with username ${username}!`,
-			);
+			this.logger.error('Invalid password.');
+			throw new UnauthorizedException('Invalid password.');
 		}
 
-		const payload = { username: user.username, sub: user._id };
+		this.logger.debug(`Admin with username ${username} found!`);
+
+		const payload = { username: user.username, sub: user.id };
 
 		this.logger.debug('Generating JWT token');
 
--- backend/src/domains/auth/auth.guard.ts
+++ backend/src/domains/auth/auth.guard.ts
@@ -27,7 +27,6 @@ export class AuthGuard implements CanActivate {
 			context.getClass(),
 		]);
 		if (isPublic) {
-			// 💡 See this condition
 			return true;
 		}
 
@@ -40,8 +39,7 @@ export class AuthGuard implements CanActivate {
 			const payload = await this.jwtService.verifyAsync(token, {
 				secret: this.configService.get<string>('JWT_SECRET'),
 			});
-			// 💡 We're assigning the payload to the request object here
-			// so that we can access it in our route handlers
+
 			request['user'] = payload;
 		} catch {
 			throw new UnauthorizedException();
