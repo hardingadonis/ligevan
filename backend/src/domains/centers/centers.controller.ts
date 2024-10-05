b73df3efa5862da58716ee5a3f1bddea52be878f
Đinh Quốc Chương <quocchuong2609@gmail.com>
2024-10-05 22:29:50 +0700

    update: implement api for `centers` module

--- backend/src/domains/centers/centers.controller.ts
+++ backend/src/domains/centers/centers.controller.ts
@@ -1,4 +1,49 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	Post,
+	Put,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { CentersService } from '@/domains/centers/centers.service';
+import {
+	CreateCenterDto,
+	UpdateCenterDto,
+} from '@/domains/centers/dto/center.dto';
 
+@ApiTags('Centers')
 @Controller('centers')
-export class CentersController {}
+export class CentersController {
+	constructor(private readonly centersService: CentersService) {}
+
+	@Post()
+	async create(@Body() createCenterDto: CreateCenterDto) {
+		return await this.centersService.create(createCenterDto);
+	}
+
+	@Get()
+	async getAll() {
+		return await this.centersService.getAll();
+	}
+
+	@Get(':id')
+	async getCenterById(@Param('id') id: string) {
+		return await this.centersService.getCenterById(id);
+	}
+
+	@Put(':id')
+	async update(
+		@Param('id') id: string,
+		@Body() updateCenterDto: UpdateCenterDto,
+	) {
+		return await this.centersService.update(id, updateCenterDto);
+	}
+
+	@Delete(':id')
+	async delete(@Param('id') id: string) {
+		return await this.centersService.delete(id);
+	}
+}
--- backend/src/domains/centers/centers.module.ts
+++ backend/src/domains/centers/centers.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
-import { CentersService } from './centers.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CenterSchema } from '@/schemas/center.schema';
+import { CentersService } from '@/domains/centers/centers.service';
+import { CentersController } from '@/domains/centers/centers.controller';
 
 @Module({
+	imports: [
+		MongooseModule.forFeature([{ name: 'Center', schema: CenterSchema }]),
+	],
+	controllers: [CentersController],
 	providers: [CentersService],
+	exports: [CentersService],
 })
 export class CentersModule {}
