a5affe6f9e1578de4d71493003739732835bdafa
Minh Vương <hardingadonis@gmail.com>
2024-10-16 22:36:26 +0700

    fix: fix always throw `InternalServerErrorException` error for `students` api

--- backend/src/domains/students/students.service.ts
+++ backend/src/domains/students/students.service.ts
@@ -1,7 +1,6 @@
 import {
 	ConflictException,
 	Injectable,
-	InternalServerErrorException,
 	Logger,
 	NotFoundException,
 } from '@nestjs/common';
@@ -23,65 +22,53 @@ export class StudentsService {
 	) {}
 
 	async create(createStudentDto: CreateStudentDto) {
-		try {
-			const existingStudent = await this.studentModel
-				.findOne({
-					email: createStudentDto.email,
-				})
-				.exec();
+		const existingStudent = await this.studentModel
+			.findOne({
+				email: createStudentDto.email,
+			})
+			.exec();
 
-			if (existingStudent) {
-				this.logger.error('Student already exists!');
+		if (existingStudent) {
+			this.logger.error('Student already exists!');
 
-				throw new ConflictException('Student already exists!');
-			}
-
-			const createStudent = new this.studentModel({
-				...createStudentDto,
-			});
-
-			this.logger.debug('Creating new student');
-
-			await createStudent.save();
-
-			this.logger.log('Student created successfully!');
-
-			return createStudent;
-		} catch (error: any) {
-			this.logger.error('Failed to create student!', error);
-
-			throw new InternalServerErrorException('Failed to create student!');
+			throw new ConflictException('Student already exists!');
 		}
+
+		const createStudent = new this.studentModel({
+			...createStudentDto,
+		});
+
+		this.logger.debug('Creating new student');
+
+		await createStudent.save();
+
+		this.logger.log('Student created successfully!');
+
+		return createStudent;
 	}
 
 	async getAll() {
-		try {
-			const students = await this.studentModel
-				.find({ isDeleted: false })
-				.select('-__v')
-				.populate({
-					select: '-__v',
-					path: 'classes',
-					model: 'Class',
-				})
-				.exec();
+		const students = await this.studentModel
+			.find({ isDeleted: false })
+			.select('-__v')
+			.populate({
+				select: '-__v',
+				path: 'classes',
+				model: 'Class',
+			})
+			.exec();
 
-			if (!students) {
-				this.logger.error('No students found!');
+		if (!students) {
+			this.logger.error('No students found!');
 
-				throw new NotFoundException('No students found!');
-			}
-
-			this.logger.debug('Found all students', students);
-
-			this.logger.log('Retrieved students');
-
-			return students;
-		} catch (error: any) {
-			this.logger.error('Failed to get all students!', error);
-
-			throw new InternalServerErrorException('Failed to get all students!');
+			throw new NotFoundException('No students found!');
 		}
+
+		this.logger.debug('Found all students', students);
+
+		this.logger.log('Retrieved students');
+
+		return students;
 	}
 
 	async getById(id: string) {
@@ -93,96 +80,78 @@ export class StudentsService {
 	}
 
 	async getStudent(conditions: object) {
-		try {
-			const student = await this.studentModel
-				.findOne({ ...conditions, isDeleted: false })
-				.select('-__v')
-				.exec();
+		const student = await this.studentModel
+			.findOne({ ...conditions, isDeleted: false })
+			.select('-__v')
+			.exec();
 
-			if (!student) {
-				this.logger.error(
-					`Student not found with conditions: ${JSON.stringify(conditions)}`,
-				);
+		if (!student) {
+			this.logger.error(
+				`Student not found with conditions: ${JSON.stringify(conditions)}`,
+			);
 
-				throw new NotFoundException(
-					'Student not found with provided conditions!',
-				);
-			}
-
-			this.logger.debug('Found student', student);
-
-			this.logger.debug('Retrieved student', student);
-
-			return student;
-		} catch (error: any) {
-			this.logger.error('Failed to get student!', error);
-
-			throw new InternalServerErrorException('Failed to get student!');
+			throw new NotFoundException(
+				'Student not found with provided conditions!',
+			);
 		}
+
+		this.logger.debug('Found student', student);
+
+		this.logger.debug('Retrieved student', student);
+
+		return student;
 	}
 
 	async update(id: string, updateStudentDto: UpdateStudentDto) {
-		try {
-			const existingStudent = await this.studentModel
-				.findOne({ _id: id, isDeleted: false })
-				.select('-__v')
-				.exec();
+		const existingStudent = await this.studentModel
+			.findOne({ _id: id, isDeleted: false })
+			.select('-__v')
+			.exec();
 
-			if (!existingStudent) {
-				this.logger.error('Student not found!');
+		if (!existingStudent) {
+			this.logger.error('Student not found!');
 
-				throw new NotFoundException('Student not found!');
-			}
-
-			this.logger.debug('Found student', existingStudent);
-
-			existingStudent.set(updateStudentDto);
-
-			this.logger.debug('Updating student');
-
-			const updatedStudent = await existingStudent.save();
-
-			this.logger.debug('Student updated', updatedStudent);
-			this.logger.log('Student updated');
-
-			return updatedStudent;
-		} catch (error: any) {
-			this.logger.error('Failed to update student!', error);
-
-			throw new InternalServerErrorException('Failed to update student!');
+			throw new NotFoundException('Student not found!');
 		}
+
+		this.logger.debug('Found student', existingStudent);
+
+		existingStudent.set(updateStudentDto);
+
+		this.logger.debug('Updating student');
+
+		const updatedStudent = await existingStudent.save();
+
+		this.logger.debug('Student updated', updatedStudent);
+		this.logger.log('Student updated');
+
+		return updatedStudent;
 	}
 
 	async delete(id: string) {
-		try {
-			const existingStudent = await this.studentModel
-				.findOne({ _id: id, isDeleted: false })
-				.select('-__v')
-				.exec();
+		const existingStudent = await this.studentModel
+			.findOne({ _id: id, isDeleted: false })
+			.select('-__v')
+			.exec();
 
-			if (!existingStudent) {
-				this.logger.error('Student not found!');
+		if (!existingStudent) {
+			this.logger.error('Student not found!');
 
-				throw new ConflictException('Student not found!');
-			}
-
-			this.logger.debug('Deleting student');
-
-			existingStudent.set({ isDeleted: true });
-
-			await existingStudent.save();
-
-			this.logger.debug('Student deleted', existingStudent);
-			this.logger.log('Student deleted');
-
-			return {
-				statusCode: 200,
-				message: 'Student deleted successfully',
-			};
-		} catch (error: any) {
-			this.logger.error('Failed to delete student!', error);
-
-			throw new InternalServerErrorException('Failed to delete student!');
+			throw new ConflictException('Student not found!');
 		}
+
+		this.logger.debug('Deleting student');
+
+		existingStudent.set({ isDeleted: true });
+
+		await existingStudent.save();
+
+		this.logger.debug('Student deleted', existingStudent);
+		this.logger.log('Student deleted');
+
+		return {
+			statusCode: 200,
+			message: 'Student deleted successfully',
+		};
 	}
 }
