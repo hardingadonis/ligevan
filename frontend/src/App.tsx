85b12db821a68ac394d9f7fe60962184f8923087
Thunder <htn10a2@gmail.com>
2024-10-16 19:01:35 +0700

    add: add lazy for loading route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HomepageStudent from '@/pages/student/Homepage';
+import { Spin } from 'antd';
+
+const HomepageStudent = lazy(() => import('@/pages/student/Homepage'));
 
 const App: React.FC = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/student" element={<HomepageStudent />} />
-      </Routes>
-    </Router>
-  );
+	return (
+		<Router>
+			<Suspense fallback={<div><Spin size="large" /> Đang tải...</div>}>
+				<Routes>
+					<Route path="/student" element={<HomepageStudent />} />
+				</Routes>
+			</Suspense>
+		</Router>
+	);
 };
 
 export default App;
