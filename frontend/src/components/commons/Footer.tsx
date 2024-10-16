6928743ffac79741c25902a5a024722832021b6f
Thunder <htn10a2@gmail.com>
2024-10-16 14:14:57 +0700

    update: update brand name in footer

--- frontend/src/components/commons/Footer.tsx
+++ frontend/src/components/commons/Footer.tsx
@@ -7,7 +7,7 @@ const { Text } = Typography;
 const Footer: React.FC = () => {
   return (
     <AntFooter style={{ textAlign: 'center', padding: '20px 50px', boxShadow: '0 -2px 8px rgba(0, 0, 0, 0.1)' }}>
-      <Text>© 2024 Ligevan.</Text>
+      <Text>© 2024 ligevan.</Text>
     </AntFooter>
   );
 };
