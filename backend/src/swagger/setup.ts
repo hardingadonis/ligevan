2c714cb23e423f5f4c5e140aaee1b9c8c0f558e8
Minh Vương <hardingadonis@gmail.com>
2024-10-17 09:51:56 +0700

    update: auto get version from `package.json`

--- backend/src/swagger/setup.ts
+++ backend/src/swagger/setup.ts
@@ -1,3 +1,4 @@
+import { version } from '../../package.json';
 import { INestApplication } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
@@ -5,7 +6,7 @@ export const setupSwagger = (app: INestApplication<any>) => {
 	const config = new DocumentBuilder()
 		.setTitle('ligevan API doumentation')
 		.setDescription('「学び、練習し、成功する」')
-		.setVersion('1.0.0')
+		.setVersion(version)
 		.setLicense(
 			'GPL-3.0 license',
 			'https://github.com/hardingadonis/ligevan/blob/main/LICENSE',
