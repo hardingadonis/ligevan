07144384971890419c9d9c1bd35d1200f1b195a5
Thunder <htn10a2@gmail.com>
2024-10-18 21:17:02 +0700

    add: responsive for Profile dropdown

--- frontend/src/components/student/DropdownProfile.tsx
+++ frontend/src/components/student/DropdownProfile.tsx
@@ -3,8 +3,9 @@ import {
 	ScheduleOutlined,
 	UserOutlined,
 } from '@ant-design/icons';
-import { Avatar, Dropdown, Menu, Spin, Typography } from 'antd';
+import { Avatar, Divider, Dropdown, Menu, Spin, Typography } from 'antd';
 import React, { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
 
 import { Student } from '@/schemas/student.schema';
 import { getStudentByEmail } from '@/services/api/student';
@@ -15,6 +16,7 @@ const { Text } = Typography;
 const DropdownProfile: React.FC = () => {
 	const [student, setStudent] = useState<Student | null>(null);
 	const [loading, setLoading] = useState<boolean>(true);
+	const isMobile = useMediaQuery({ maxWidth: 767 });
 
 	useEffect(() => {
 		const fetchStudent = async () => {
@@ -58,14 +60,22 @@ const DropdownProfile: React.FC = () => {
 
 	const menu = (
 		<Menu onClick={handleMenuClick}>
+			{isMobile && (
+				<>
+					<Menu.Item key="studentName" disabled>
+						{student?.fullName}
+					</Menu.Item>
+					<Menu.Divider />
+				</>
+			)}
 			<Menu.Item key="profile" icon={<UserOutlined />}>
-				Profile
+				Hồ sơ
 			</Menu.Item>
 			<Menu.Item key="schedule" icon={<ScheduleOutlined />}>
-				Schedule
+				Lịch học
 			</Menu.Item>
 			<Menu.Item key="logout" icon={<LogoutOutlined />}>
-				Logout
+				Đăng xuất
 			</Menu.Item>
 		</Menu>
 	);
@@ -77,9 +87,14 @@ const DropdownProfile: React.FC = () => {
 	return (
 		<Dropdown overlay={menu} trigger={['hover']}>
 			<div style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}>
-				<Text style={{ marginRight: 8, color: 'white' }} className="user-name">
-					{student?.fullName}
-				</Text>
+				{!isMobile && (
+					<Text
+						style={{ marginRight: 8, color: 'white' }}
+						className="user-name"
+					>
+						{student?.fullName}
+					</Text>
+				)}
 				<Avatar
 					src={student?.avatar}
 					icon={!student?.avatar && <UserOutlined />}
