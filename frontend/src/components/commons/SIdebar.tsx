ea5e8be7c43154be5f5059a3d5d9e98c87b0c9b9
linhlm <lammylinh7012@gmail.com>
2024-10-19 14:46:13 +0700

    add: add login page for teacher
    Add login page for teacher

--- frontend/src/components/commons/SIdebar.tsx
+++ frontend/src/components/commons/SIdebar.tsx
@@ -1,42 +1,41 @@
-import React from 'react';
 import { Menu } from 'antd';
 import type { MenuProps } from 'antd';
+import React from 'react';
 
 export type MenuItem = Required<MenuProps>['items'][number];
 
 interface SidebarProps {
-  defaultSelectedKey?: string;
-  items: MenuItem[];
-  handleMenuClick: (key: string) => void;
+	defaultSelectedKey?: string;
+	items: MenuItem[];
+	handleMenuClick: (key: string) => void;
 }
 
-const Sidebar: React.FC<SidebarProps> = ({ 
-  defaultSelectedKey = 'dashboard', 
-  items, 
-  handleMenuClick 
+const Sidebar: React.FC<SidebarProps> = ({
+	defaultSelectedKey = 'dashboard',
+	items,
+	handleMenuClick,
 }) => {
-
-  return (
-    <Menu
-      mode="vertical"
-      defaultSelectedKeys={[defaultSelectedKey]}
-      onClick={({ key }) => handleMenuClick(key as string)}
-      style={{
-        width: 250,
-        height: '100vh',
-        position: 'fixed',
-        top: 0,
-        left: 0,
-        borderRight: '1px solid #e0e0e0',
-        backgroundColor: '#f0f0f0',
-        color: '#000',
-        fontSize: '16px',
-        fontWeight: 500,
-        marginTop: 65
-      }}
-      items={items}
-    />
-  );
+	return (
+		<Menu
+			mode="vertical"
+			defaultSelectedKeys={[defaultSelectedKey]}
+			onClick={({ key }) => handleMenuClick(key as string)}
+			style={{
+				width: 250,
+				height: '100vh',
+				position: 'fixed',
+				top: 0,
+				left: 0,
+				borderRight: '1px solid #e0e0e0',
+				backgroundColor: '#f0f0f0',
+				color: '#000',
+				fontSize: '16px',
+				fontWeight: 500,
+				marginTop: 65,
+			}}
+			items={items}
+		/>
+	);
 };
 
 export default Sidebar;
