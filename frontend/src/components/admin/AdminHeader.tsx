067f5df15253416c0eb402e98130d4a27b7b54dc
Bun He <hungoffsize@gmail.com>
2024-10-19 20:47:17 +0700

    fix: fix header admin

--- frontend/src/components/admin/AdminHeader.tsx
+++ frontend/src/components/admin/AdminHeader.tsx
@@ -1,9 +1,10 @@
 import Header from '../commons/Header';
-import { DownOutlined, UserOutlined } from '@ant-design/icons';
-import { Avatar, Dropdown, Menu, message } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Dropdown, Menu, Typography, message } from 'antd';
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+const { Text } = Typography;
 const HeaderAdmin: React.FC = () => {
 	const navigate = useNavigate();
 	const fullName = localStorage.getItem('adminFullName');
@@ -25,15 +26,16 @@ const HeaderAdmin: React.FC = () => {
 
 	const rightComponent = accessToken ? (
 		<>
-			<span style={{ color: 'white', paddingRight: '15px' }}>{fullName}</span>
 			<Dropdown overlay={menu} trigger={['click']}>
 				<div>
+					<Text style={{ color: 'white', paddingRight: '15px' }}>
+						{fullName}
+					</Text>
 					<Avatar
 						style={{ marginRight: '8px', backgroundColor: 'white' }}
 						size="large"
 						icon={<UserOutlined style={{ color: 'black' }} />}
 					/>
-					<DownOutlined style={{ color: 'white' }} />
 				</div>
 			</Dropdown>
 		</>
