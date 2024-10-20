a17bb200fed28436dfba532e59540e361f43bf4a
Thunder <htn10a2@gmail.com>
2024-10-20 17:08:14 +0700

    update: student page layout with side bar

--- frontend/src/layouts/student/student.haveSidebar.tsx
+++ frontend/src/layouts/student/student.haveSidebar.tsx
@@ -1,12 +1,11 @@
-import { HistoryOutlined, TeamOutlined, UserOutlined } from '@ant-design/icons';
 import { Col, FloatButton, Layout, Row } from 'antd';
 import React from 'react';
 
 import '@/assets/styles/dropdownCenter.css';
 import Footer from '@/components/commons/Footer';
 import Header from '@/components/commons/Header';
-import Sidebar, { MenuItem } from '@/components/commons/Sidebar';
 import DropdownProfile from '@/components/student/DropdownProfile';
+import StudentSidebar from '@/components/student/Sidebar';
 
 const { Content } = Layout;
 
@@ -19,36 +18,11 @@ const StudentLayoutWithSidebar: React.FC<StudentLayoutProps> = ({
 }) => {
 	const rightComponent = <DropdownProfile />;
 
-	const menuItems: MenuItem[] = [
-		{
-			key: 'profile',
-			icon: <UserOutlined />,
-			label: 'Hồ sơ',
-			// link: '/student/profile',
-		},
-		{
-			key: 'classList',
-			icon: <TeamOutlined />,
-			label: 'Danh sách lớp',
-			// link: '/student/classes',
-		},
-		{
-			key: 'paymentHistory',
-			icon: <HistoryOutlined />,
-			label: 'Lịch sử thanh toán',
-			// link: '/student/payment-history',
-		},
-	];
-
-	const handleMenuClick = (key: string) => {
-		console.log('Menu item clicked:', key);
-	};
-
 	return (
 		<Layout style={{ minHeight: '100vh', position: 'relative' }}>
 			<Header rightComponent={rightComponent} />
 			<Layout>
-				<Sidebar items={menuItems} handleMenuClick={handleMenuClick} />
+				<StudentSidebar />
 				<Layout style={{ paddingLeft: 250 }}>
 					<Content style={{ margin: '24px 16px 0' }}>
 						<Row justify="center">
