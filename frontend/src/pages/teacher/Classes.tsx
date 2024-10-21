e6dff167e993105273e1bc8e99a04d217cbebd30
Thunder <htn10a2@gmail.com>
2024-10-21 09:32:38 +0700

    fix: error in class list for teacher page
    split to component Class List and then import in Class Manager page

--- frontend/src/pages/teacher/Classes.tsx
+++ frontend/src/pages/teacher/Classes.tsx
@@ -1,12 +1,9 @@
-import { Alert, Button, Card, Col, Input, Row, Spin, Table } from 'antd';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 
 import TeacherLayout from '@/layouts/teacher';
-import { Class } from '@/schemas/class.schema';
-import { Teacher } from '@/schemas/teacher.schema';
 import {
 	selectEmail,
 	selectToken,
@@ -14,6 +11,8 @@ import {
 	setFullName,
 } from '@/slices/teacher';
 import { apiBaseUrl } from '@/utils/apiBase';
+
+import ListClass from './ListClass';
 
 const ClassesPage: React.FC = () => {
 	const email = useSelector(selectEmail);
@@ -41,147 +40,12 @@ const ClassesPage: React.FC = () => {
 		setIsMounted(true);
 	}, [token, email, dispatch, navigate]);
 
-	const [classes, setClasses] = useState<Class[]>([]);
-	const [loading, setLoading] = useState<boolean>(false);
-	const [error, setError] = useState<string | null>(null);
-	const [searchValue, setSearchValue] = useState<string>('');
-	const [filteredClasses, setFilteredClasses] = useState<Class[]>([]);
-
-	useEffect(() => {
-		const fetchClassesForTeacher = async () => {
-			setLoading(true);
-			try {
-				const teacherResponse = await axios.get<Teacher>(
-					apiBaseUrl + `/api/teachers/email/${email}`,
-				);
-				const teacher = teacherResponse.data;
-
-				const teacherClasses = teacher.classes || [];
-
-				const classRequests = teacherClasses.map((cls) =>
-					axios.get<Class>(apiBaseUrl + `/api/classes/${cls}`),
-				);
-
-				const classResponses = await Promise.all(classRequests);
-
-				const fullClasses = classResponses.map((response) => response.data);
-				setClasses(fullClasses);
-				setFilteredClasses(fullClasses);
-			} catch (err: any) {
-				setError('Không thể tải danh sách lớp học');
-			} finally {
-				setLoading(false);
-			}
-		};
-
-		fetchClassesForTeacher();
-	}, [email]);
-
-	useEffect(() => {
-		const filtered = classes.filter((cls) =>
-			cls.name.toLowerCase().includes(searchValue.toLowerCase()),
-		);
-		setFilteredClasses(filtered);
-	}, [searchValue, classes]);
-
-	const columns = [
-		{
-			title: 'STT',
-			dataIndex: 'index',
-			key: 'index',
-			render: (_: any, __: any, index: number) => index + 1,
-			align: 'center',
-		},
-		{
-			title: 'Tên Lớp',
-			dataIndex: 'name',
-			key: 'name',
-			align: 'center',
-		},
-		{
-			title: 'Học sinh',
-			dataIndex: 'students',
-			key: 'students',
-			render: (students: { fullName: string }[]) => students.length,
-			align: 'center',
-		},
-		{
-			title: 'Tiết học',
-			dataIndex: 'slots',
-			key: 'slots',
-			render: (slots: { fullName: string }[]) => slots.length,
-			align: 'center',
-		},
-		{
-			title: 'Khóa học',
-			dataIndex: ['course', 'title'],
-			key: 'courses',
-			align: 'center',
-		},
-		{
-			title: 'Thao tác',
-			key: 'action',
-			render: (_: any, record: Class) => (
-				<Button
-					color="default"
-					variant="solid"
-					type="primary"
-					onClick={() => navigate(`/teacher/classes/${record._id}`)}
-				>
-					Xem chi tiết
-				</Button>
-			),
-			align: 'center',
-		},
-	];
-
 	if (!isMounted) {
 		return null;
 	} else
 		return (
 			<TeacherLayout>
-				<Row justify="center" style={{ padding: '16px 0' }}>
-					<Col xs={24} lg={20}>
-						<Card
-							bordered={false}
-							style={{ boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)' }}
-						>
-							<h2 style={{ textAlign: 'center' }}>DANH SÁCH CÁC LỚP HỌC</h2>
-							<Row justify="end" style={{ marginBottom: 16 }}>
-								<Col>
-									<Input
-										placeholder="Tìm kiếm"
-										value={searchValue}
-										onChange={(e) => setSearchValue(e.target.value)}
-										style={{ width: 200 }}
-									/>
-								</Col>
-							</Row>
-							{loading && <Spin size="large" />}
-							{error && (
-								<Alert
-									message="Lỗi"
-									description={error}
-									type="error"
-									showIcon
-								/>
-							)}
-							{!loading && !error && (
-								<Table
-									columns={columns}
-									dataSource={filteredClasses}
-									rowKey="_id"
-									pagination={{ pageSize: 10 }}
-									locale={{
-										emptyText: 'Không có kết quả tìm kiếm',
-									}}
-									scroll={{ x: 'max-content' }}
-									style={{ whiteSpace: 'nowrap' }}
-								/>
-							)}
-						</Card>
-					</Col>
-				</Row>
+				<ListClass email={email || ''} />
 			</TeacherLayout>
 		);
 };
