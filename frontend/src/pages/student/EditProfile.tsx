6e7306ad479424d7cb6a54d8ea15e5941fc924c6
Thunder <htn10a2@gmail.com>
2024-10-21 19:31:29 +0700

    update: import component Form Update into page

--- frontend/src/pages/student/EditProfile.tsx
+++ frontend/src/pages/student/EditProfile.tsx
@@ -1,9 +1,10 @@
+import FormUpdate from '@/components/student/FormUpdateProfile';
 import StudentLayoutWithSidebar from '@/layouts/student/student.haveSidebar';
 
 const EditProfileStudent: React.FC = () => {
 	return (
 		<StudentLayoutWithSidebar>
-			<h1>Edit Profile</h1>
+			<FormUpdate />
 		</StudentLayoutWithSidebar>
 	);
 };
