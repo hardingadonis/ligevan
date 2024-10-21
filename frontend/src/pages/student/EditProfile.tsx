d04d8a9d2c54edb4d4b89ec2b256ec7e234ede68
Thunder <htn10a2@gmail.com>
2024-10-21 22:51:42 +0700

    update: import Button go back
    For two pages Student Detail and Student Update Detail

--- frontend/src/pages/student/EditProfile.tsx
+++ frontend/src/pages/student/EditProfile.tsx
@@ -1,10 +1,20 @@
+import { Col, Row } from 'antd';
+
+import ButtonGoBack from '@/components/commons/ButtonGoback';
 import FormUpdate from '@/components/student/FormUpdateProfile';
 import StudentLayoutWithSidebar from '@/layouts/student/student.haveSidebar';
 
 const EditProfileStudent: React.FC = () => {
 	return (
 		<StudentLayoutWithSidebar>
-			<FormUpdate />
+			<Row>
+				<Col span={2}>
+					<ButtonGoBack />
+				</Col>
+				<Col span={22}>
+					<FormUpdate />
+				</Col>
+			</Row>
 		</StudentLayoutWithSidebar>
 	);
 };
--- frontend/src/pages/student/Profile.tsx
+++ frontend/src/pages/student/Profile.tsx
@@ -1,10 +1,20 @@
+import { Col, Row } from 'antd';
+
+import ButtonGoBack from '@/components/commons/ButtonGoback';
 import DetailStudent from '@/components/student/DetailStudent';
 import StudentLayoutWithSidebar from '@/layouts/student/student.haveSidebar';
 
 const StudentProfile: React.FC = () => {
 	return (
 		<StudentLayoutWithSidebar>
-			<DetailStudent />
+			<Row>
+				<Col span={2}>
+					<ButtonGoBack />
+				</Col>
+				<Col span={22}>
+					<DetailStudent />
+				</Col>
+			</Row>
 		</StudentLayoutWithSidebar>
 	);
 };
