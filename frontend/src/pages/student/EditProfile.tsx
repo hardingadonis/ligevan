6d284b51609ece56a01f56542112bab57a7f54cf
Thunder <htn10a2@gmail.com>
2024-10-21 18:28:34 +0700

    add: student profile edit page

---
+++ frontend/src/pages/student/EditProfile.tsx
@@ -0,0 +1,11 @@
+import StudentLayoutWithSidebar from '@/layouts/student/student.haveSidebar';
+
+const EditProfileStudent: React.FC = () => {
+	return (
+		<StudentLayoutWithSidebar>
+			<h1>Edit Profile</h1>
+		</StudentLayoutWithSidebar>
+	);
+};
+
+export default EditProfileStudent;
