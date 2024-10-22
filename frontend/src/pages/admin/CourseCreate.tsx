000e66e50f570465a054c7fa06eafbeb86262c39
Bun He <hungoffsize@gmail.com>
2024-10-22 17:54:22 +0700

    add: add page to create course

---
+++ frontend/src/pages/admin/CourseCreate.tsx
@@ -0,0 +1,31 @@
+import { ArrowLeftOutlined } from '@ant-design/icons';
+import { Button, Space } from 'antd';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import CourseForm from '@/components/admin/CourseForm';
+import AdminLayout from '@/layouts/admin';
+
+const CreateCoursePage: React.FC = () => {
+	const navigate = useNavigate();
+
+	const handleGoBack = () => {
+		navigate('/admin/courses');
+	};
+
+	return (
+		<AdminLayout>
+			<div
+				style={{
+					maxWidth: 800,
+					margin: '0 auto',
+					padding: '24px',
+				}}
+			>
+				<CourseForm onSuccess={handleGoBack} />
+			</div>
+		</AdminLayout>
+	);
+};
+
+export default CreateCoursePage;
