13794e1b22960667429636b165c31b67a11f0132
Thunder <htn10a2@gmail.com>
2024-10-22 16:46:25 +0700

    update: class list page for student

--- frontend/src/pages/student/ClassList.tsx
+++ frontend/src/pages/student/ClassList.tsx
@@ -1,9 +1,10 @@
+import ListClass from '@/components/student/ListClass';
 import StudentLayoutWithSidebar from '@/layouts/student/student.haveSidebar';
 
 const StudentClassList: React.FC = () => {
 	return (
 		<StudentLayoutWithSidebar>
-			<h2>Class List</h2>
+			<ListClass />
 		</StudentLayoutWithSidebar>
 	);
 };
