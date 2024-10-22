202e39d8d693d42dd032d9d867ce8996f38bc287
Quốc Chương <quocchuong2609@gmail.com>
2024-10-22 15:22:22 +0700

    add: `voucher` detail component

---
+++ frontend/src/components/admin/VoucherDetail.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const VoucherDetail: React.FC = () => {
+	return (
+		<>
+			<h1 style={{ textAlign: 'center' }}>Chi tiết mã giảm giá</h1>
+		</>
+	);
+};
+
+export default VoucherDetail;
