893890875e298850d2be10bc6259071998f798e9
linhlm <lammylinh7012@gmail.com>
2024-10-22 19:09:22 +0700

    add: fix yarn build
    fix yarn build

--- frontend/src/components/teacher/ListStudent.tsx
+++ frontend/src/components/teacher/ListStudent.tsx
@@ -15,7 +15,6 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import '@/assets/styles/ListCenters.css';
 import { Class } from '@/schemas/class.schema';
 import { Student } from '@/schemas/student.schema';
 import { apiBaseUrl } from '@/utils/apiBase';
