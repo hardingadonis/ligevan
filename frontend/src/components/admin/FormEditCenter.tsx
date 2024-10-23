712569dd1ad3adfae15ca8241383c2cf1a3510cb
Thunder <htn10a2@gmail.com>
2024-10-23 20:05:30 +0700

    update: implement Redux in Form edit center

--- frontend/src/components/admin/FormEditCenter.tsx
+++ frontend/src/components/admin/FormEditCenter.tsx
@@ -1,15 +1,34 @@
-import { Button, Form, Input, message } from 'antd';
-import React, { useEffect, useState } from 'react';
+import ButtonGoBack from '../commons/ButtonGoback';
+import { SaveOutlined } from '@ant-design/icons';
+import { Button, Col, Form, Input, Row, message } from 'antd';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { Center } from '@/schemas/center.schema';
-import { getCenterById, updateCenter } from '@/services/api/center';
+import { getCenterById } from '@/services/api/center';
+import {
+	updateCenterById,
+	updateCenterFulfilled,
+	updateCenterPending,
+	updateCenterRejected,
+} from '@/slices/center';
+import store from '@/store';
+import {
+	validatePhoneNumber,
+	validateVietnameseAddress,
+	validateVietnamesePhoneNumber,
+} from '@/utils/inputValidate';
+
+type RootState = ReturnType<typeof store.getState>;
+type AppDispatch = typeof store.dispatch;
 
 const FormEditCenter: React.FC = () => {
 	const { id } = useParams<{ id: string }>();
 	const navigate = useNavigate();
+	const dispatch = useDispatch<AppDispatch>();
 	const [form] = Form.useForm();
-	const [loading, setLoading] = useState(false);
+	const { loading } = useSelector((state: RootState) => state.centers);
 
 	useEffect(() => {
 		const fetchCenter = async () => {
@@ -28,86 +47,132 @@ const FormEditCenter: React.FC = () => {
 	}, [id, form]);
 
 	const onFinish = async (values: Partial<Center>) => {
-		setLoading(true);
-		try {
-			if (id) {
-				await updateCenter(id, values);
-			} else {
-				message.error('ID trung tâm không hợp lệ');
-			}
-			message.success('Cập nhật trung tâm thành công');
-			navigate('/admin/centers');
-		} catch {
-			message.error('Cập nhật trung tâm thất bại');
-		} finally {
-			setLoading(false);
+		if (id) {
+			dispatch(updateCenterPending());
+			dispatch(updateCenterById({ id, values }))
+				.unwrap()
+				.then(() => {
+					dispatch(updateCenterFulfilled());
+					message.success('Cập nhật trung tâm thành công.');
+					navigate('/admin/centers');
+				})
+				.catch((error: { message: string }) => {
+					dispatch(updateCenterRejected(error.message));
+					message.error('Cập nhật trung tâm thất bại!');
+				});
+		} else {
+			message.error('ID trung tâm không hợp lệ!');
 		}
 	};
 
+	const validatePhone = (_: unknown, value: string) => {
+		if (!value) {
+			return Promise.reject(
+				new Error('Vui lòng nhập số điện thoại trung tâm!'),
+			);
+		}
+		if (!validatePhoneNumber(value)) {
+			return Promise.reject(new Error('Số điện thoại không hợp lệ!'));
+		}
+		if (!validateVietnamesePhoneNumber(value)) {
+			return Promise.reject(
+				new Error('Số điện thoại không đúng định dạng Việt Nam!'),
+			);
+		}
+		return Promise.resolve();
+	};
+
 	return (
-		<div
-			style={{
-				display: 'flex',
-				flexDirection: 'column',
-				alignItems: 'center',
-				padding: '65px 20px 0 270px',
-			}}
-		>
-			<h2 style={{ textAlign: 'center', marginBottom: '20px' }}>
-				Chỉnh sửa thông tin trung tâm
-			</h2>
+		<div style={{ padding: '65px 20px 0 270px' }}>
+			<Row>
+				<Col span={2}>
+					<ButtonGoBack link="/admin/centers" />
+				</Col>
+				<Col span={20}>
+					<h2 style={{ textAlign: 'center', marginBottom: '20px' }}>
+						Chỉnh sửa thông tin trung tâm
+					</h2>
+				</Col>
+			</Row>
 			<div
 				style={{
-					maxWidth: '800px',
-					boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-					padding: '20px',
-					borderRadius: '8px',
-					backgroundColor: '#f5f5f5',
-					width: '100%',
+					display: 'flex',
+					justifyContent: 'center',
+					alignItems: 'center',
 				}}
 			>
-				<Form
-					form={form}
-					onFinish={onFinish}
-					layout="horizontal"
-					labelCol={{ span: 4 }}
-					wrapperCol={{ span: 20 }}
-					labelAlign="left"
+				<div
+					style={{
+						maxWidth: '800px',
+						boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
+						padding: '20px',
+						borderRadius: '8px',
+						backgroundColor: '#f5f5f5',
+						width: '100%',
+					}}
 				>
-					<Form.Item
-						name="name"
-						label="Tên"
-						rules={[{ required: true, message: 'Vui lòng nhập tên' }]}
+					<Form
+						form={form}
+						onFinish={onFinish}
+						layout="horizontal"
+						labelCol={{ span: 4 }}
+						wrapperCol={{ span: 20 }}
+						labelAlign="left"
+						style={{ margin: '15px 20px' }}
 					>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						name="email"
-						label="Email"
-						rules={[{ required: true, message: 'Vui lòng nhập email' }]}
-					>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						name="address"
-						label="Địa chỉ"
-						rules={[{ required: true, message: 'Vui lòng nhập địa chỉ' }]}
-					>
-						<Input />
-					</Form.Item>
-					<Form.Item
-						name="phone"
-						label="Số điện thoại"
-						rules={[{ required: true, message: 'Vui lòng nhập số điện thoại' }]}
-					>
-						<Input />
-					</Form.Item>
-					<Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
-						<Button type="primary" htmlType="submit" loading={loading}>
-							Cập nhật
-						</Button>
-					</Form.Item>
-				</Form>
+						<Form.Item
+							name="name"
+							label="Tên"
+							rules={[
+								{ required: true, message: 'Vui lòng nhập tên trung tâm!' },
+							]}
+						>
+							<Input />
+						</Form.Item>
+						<Form.Item
+							name="email"
+							label="Email"
+							rules={[
+								{ required: true, message: 'Vui lòng nhập email trung tâm!' },
+								{ type: 'email', message: 'Email không hợp lệ!' },
+							]}
+						>
+							<Input />
+						</Form.Item>
+						<Form.Item
+							name="address"
+							label="Địa chỉ"
+							rules={[
+								{ required: true, message: 'Vui lòng nhập địa chỉ trung tâm!' },
+								{
+									validator: (_, value) =>
+										validateVietnameseAddress(value)
+											? Promise.resolve()
+											: Promise.reject(new Error('Địa chỉ không hợp lệ!')),
+								},
+							]}
+						>
+							<Input />
+						</Form.Item>
+						<Form.Item
+							name="phone"
+							label="Số điện thoại"
+							rules={[{ required: true }, { validator: validatePhone }]}
+						>
+							<Input />
+						</Form.Item>
+						<Form.Item style={{ display: 'flex', justifyContent: 'flex-end' }}>
+							<Button
+								type="primary"
+								htmlType="submit"
+								loading={loading}
+								icon={<SaveOutlined />}
+							>
+								Lưu Lại
+							</Button>
+						</Form.Item>
+					</Form>
+				</div>
 			</div>
 		</div>
 	);
