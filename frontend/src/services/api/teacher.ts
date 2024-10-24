fbe01ff7fd98ed2943a31c79c01a510c71eede0c
Bun He <hungoffsize@gmail.com>
2024-10-24 17:42:59 +0700

    add: add api get teacher by email

--- frontend/src/services/api/teacher.ts
+++ frontend/src/services/api/teacher.ts
@@ -12,3 +12,15 @@ export const getAllTeacher = async (): Promise<Teacher[]> => {
 		throw error;
 	}
 };
+
+export const getTeacherByEmail = async (email: string): Promise<Teacher> => {
+	try {
+		const response = await axios.get(
+			`${apiBaseUrl}/api/teachers/email/${email}`,
+		);
+		return response.data;
+	} catch (error) {
+		console.error('Error fetching teacher:', error);
+		throw error;
+	}
+};
