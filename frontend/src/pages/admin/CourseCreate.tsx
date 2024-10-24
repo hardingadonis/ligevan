24c56f5d8d523ca69acd958364e27d9d804c6563
Quốc Chương <quocchuong2609@gmail.com>
2024-10-24 20:05:12 +0700

    update: file name & layout course detail

--- frontend/src/pages/admin/CourseCreate.tsx
+++ frontend/src/pages/admin/CourseCreate.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import CourseForm from '@/components/admin/CourseForm';
+import CourseForm from '@/components/admin/FormCreateCourse';
 import AdminLayout from '@/layouts/admin';
 
 const CreateCoursePage: React.FC = () => {
@@ -13,15 +13,7 @@ const CreateCoursePage: React.FC = () => {
 
 	return (
 		<AdminLayout>
-			<div
-				style={{
-					maxWidth: 800,
-					margin: '0 auto',
-					padding: '24px',
-				}}
-			>
-				<CourseForm onSuccess={handleGoBack} />
-			</div>
+			<CourseForm onSuccess={handleGoBack} />
 		</AdminLayout>
 	);
 };
