ac49e9a975ebb415642963664f00268020f71bc2
Quốc Chương <quocchuong2609@gmail.com>
2024-10-30 20:10:02 +0700

    update: handle go back

--- frontend/src/components/commons/ButtonGoback.tsx
+++ frontend/src/components/commons/ButtonGoback.tsx
@@ -4,14 +4,17 @@ import { useNavigate } from 'react-router-dom';
 
 interface ButtonGoBackProps {
 	link?: string;
+	history?: boolean;
 }
 
-const ButtonGoBack: React.FC<ButtonGoBackProps> = ({ link }) => {
+const ButtonGoBack: React.FC<ButtonGoBackProps> = ({ link, history }) => {
 	const navigate = useNavigate();
 
 	const handleGoBack = () => {
 		if (link) {
 			navigate(link);
+		} else if (history) {
+			window.history.back();
 		} else {
 			const currentPath = window.location.pathname;
 			const newPath = currentPath.substring(0, currentPath.lastIndexOf('/'));
