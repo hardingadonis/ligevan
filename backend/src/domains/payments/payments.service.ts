80c98c3402869465660f95d239e00ca020068090
Minh Vương <hardingadonis@gmail.com>
2024-10-30 10:32:39 +0700

    update: auto add data after student register course

--- backend/src/domains/payments/payments.service.ts
+++ backend/src/domains/payments/payments.service.ts
@@ -8,7 +8,9 @@ import {
 } from '@/domains/payments/dto/payment.dto';
 import { MomoService } from '@/domains/payments/momo/momo.service';
 import { ZalopayService } from '@/domains/payments/zalopay/zalopay.service';
+import { Class } from '@/schemas/class.schema';
 import { Payment } from '@/schemas/payment.schema';
+import { Student } from '@/schemas/student.schema';
 
 @Injectable()
 export class PaymentsService {
@@ -16,6 +18,8 @@ export class PaymentsService {
 
 	constructor(
 		@InjectModel(Payment.name) private readonly salaryModel: Model<Payment>,
+		@InjectModel(Student.name) private readonly studentModel: Model<Student>,
+		@InjectModel(Class.name) private readonly classModel: Model<Class>,
 		private readonly zalopayService: ZalopayService,
 		private readonly momoService: MomoService,
 	) {}
@@ -43,10 +47,48 @@ export class PaymentsService {
 
 		this.logger.debug('Creating new payment');
 
-		await newPayment.save();
-
 		this.logger.debug('Payment created', newPayment);
 		this.logger.log('Payment created');
+
+		const student = await this.studentModel
+			.findById(createPaymentDto.student)
+			.exec();
+
+		if (
+			student.payments.includes(newPayment._id) &&
+			student.classes.includes(newPayment.class)
+		) {
+			this.logger.error('Payment ID already exists in student payments array!');
+			throw new ConflictException(
+				'Payment ID already exists in student payments array!',
+			);
+		}
+
+		this.logger.debug('Added payment to student');
+
+		const classDoc = await this.classModel
+			.findById(createPaymentDto.class)
+			.exec();
+
+		if (classDoc.students.includes(newPayment.student)) {
+			this.logger.error('Student already exists in class!');
+			throw new ConflictException('Student already exists in class!');
+		}
+
+		await this.studentModel.findByIdAndUpdate(createPaymentDto.student, {
+			$push: {
+				classes: newPayment.class.toString(),
+				payments: newPayment._id.toString(),
+			},
+		});
+
+		await this.classModel.findByIdAndUpdate(createPaymentDto.class, {
+			$push: { students: newPayment.student },
+		});
+
+		await newPayment.save();
+
+		this.logger.debug('Added student to class');
 
 		switch (createPaymentDto.method) {
 			case 'zalo-pay': {
