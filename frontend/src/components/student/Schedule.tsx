4bf891d5e70618dbd51af40cdb65775ade2a0d68
Thunder <htn10a2@gmail.com>
2024-10-30 17:50:07 +0700

    update: fix sonar cloud issues and add refresh button

--- frontend/src/components/student/Schedule.tsx
+++ frontend/src/components/student/Schedule.tsx
@@ -2,6 +2,7 @@ import {
 	CheckCircleOutlined,
 	ClockCircleOutlined,
 	CloseCircleOutlined,
+	SyncOutlined,
 } from '@ant-design/icons';
 import { L10n, loadCldr } from '@syncfusion/ej2-base';
 import '@syncfusion/ej2-base/styles/material.css';
@@ -22,7 +23,7 @@ import {
 } from '@syncfusion/ej2-react-schedule';
 import '@syncfusion/ej2-react-schedule/styles/material.css';
 import '@syncfusion/ej2-splitbuttons/styles/material.css';
-import { Col, Row, Tag, Typography } from 'antd';
+import { Button, Col, Row, Tag, Typography } from 'antd';
 import * as gregorian from 'cldr-data/main/vi/ca-gregorian.json';
 import * as numberingSystems from 'cldr-data/main/vi/numbers.json';
 import * as timeZoneNames from 'cldr-data/main/vi/timeZoneNames.json';
@@ -101,36 +102,37 @@ const SetupSchedules: React.FC = () => {
 		fetchCenters();
 	}, []);
 
-	useEffect(() => {
-		const fetchSlots = async () => {
-			try {
-				const email = (await fetchStudentData()).email;
-				const allSlots = await getSlotsByStudentEmail(email);
-				const convertedData: ExtendedSlot[] = await Promise.all(
-					allSlots.map(async (slot) => {
-						const center = await getCenterById(slot.class.center.toString());
-						const status = await checkAttendanceStatus(email, slot._id);
-						let description = 'Chưa học';
-						if (status === 'attended') {
-							description = 'Đã điểm danh';
-						} else if (status === 'absent') {
-							description = 'Vắng mặt';
-						}
+	const fetchSlots = async () => {
+		try {
+			const email = (await fetchStudentData()).email;
+			const allSlots = await getSlotsByStudentEmail(email);
+			const convertedData: ExtendedSlot[] = await Promise.all(
+				allSlots.map(async (slot) => {
+					const center = await getCenterById(slot.class.center.toString());
+					const status = await checkAttendanceStatus(email, slot._id);
+					let description = 'Chưa học';
+					if (status === 'attended') {
+						description = 'Đã điểm danh';
+					} else if (status === 'absent') {
+						description = 'Vắng mặt';
+					}
 
-						return {
-							...slot,
-							start: convertToUTC(new Date(slot.start)),
-							end: convertToUTC(new Date(slot.end)),
-							centerName: center.name,
-							Description: description,
-						};
-					}),
-				);
-				setSlots(convertedData);
-			} catch (error) {
-				console.error('Error fetching slots:', error);
-			}
-		};
+					return {
+						...slot,
+						start: convertToUTC(new Date(slot.start)),
+						end: convertToUTC(new Date(slot.end)),
+						centerName: center.name,
+						Description: description,
+					};
+				}),
+			);
+			setSlots(convertedData);
+		} catch (error) {
+			console.error('Error fetching slots:', error);
+		}
+	};
+
+	useEffect(() => {
 		fetchSlots();
 	}, []);
 
@@ -153,6 +155,10 @@ const SetupSchedules: React.FC = () => {
 
 	const handleCourseChange = (value: string) => {
 		setSelectedCourse(value);
+	};
+
+	const handleRefresh = () => {
+		fetchSlots();
 	};
 
 	const filteredSlots = slots.filter((slot) => {
@@ -184,53 +190,52 @@ const SetupSchedules: React.FC = () => {
 		Id: string;
 		Location: string;
 		CenterName: string;
-	}) => (
-		<div style={{ padding: '5px' }}>
-			<div>
-				<h3>{props.Subject}</h3>
+	}) => {
+		let icon;
+		let color;
+
+		if (props.Description === 'Đã điểm danh') {
+			icon = <CheckCircleOutlined />;
+			color = 'success';
+		} else if (props.Description === 'Vắng mặt') {
+			icon = <CloseCircleOutlined />;
+			color = 'error';
+		} else {
+			icon = <ClockCircleOutlined />;
+			color = 'default';
+		}
+
+		return (
+			<div style={{ padding: '5px' }}>
+				<div>
+					<h3>{props.Subject}</h3>
+				</div>
+				<div style={{ marginTop: '7px' }}>
+					{new Date(props.StartTime).toLocaleTimeString([], {
+						hour: '2-digit',
+						minute: '2-digit',
+					})}{' '}
+					-{' '}
+					{new Date(props.EndTime).toLocaleTimeString([], {
+						hour: '2-digit',
+						minute: '2-digit',
+					})}
+				</div>
+				<div style={{ marginTop: '5px' }}>
+					<strong>{props.CenterName}</strong>
+				</div>
+				<div style={{ marginTop: '5px' }}>
+					<strong>Tại phòng: </strong>
+					{props.Location}
+				</div>
+				<div style={{ marginTop: '10px' }}>
+					<Tag icon={icon} color={color}>
+						{props.Description}
+					</Tag>
+				</div>
 			</div>
-			<div style={{ marginTop: '7px' }}>
-				{new Date(props.StartTime).toLocaleTimeString([], {
-					hour: '2-digit',
-					minute: '2-digit',
-				})}{' '}
-				-{' '}
-				{new Date(props.EndTime).toLocaleTimeString([], {
-					hour: '2-digit',
-					minute: '2-digit',
-				})}
-			</div>
-			<div style={{ marginTop: '5px' }}>
-				<strong>{props.CenterName}</strong>
-			</div>
-			<div style={{ marginTop: '5px' }}>
-				<strong>Tại phòng: </strong>
-				{props.Location}
-			</div>
-			<div style={{ marginTop: '10px' }}>
-				<Tag
-					icon={
-						props.Description === 'Đã điểm danh' ? (
-							<CheckCircleOutlined />
-						) : props.Description === 'Vắng mặt' ? (
-							<CloseCircleOutlined />
-						) : (
-							<ClockCircleOutlined />
-						)
-					}
-					color={
-						props.Description === 'Đã điểm danh'
-							? 'success'
-							: props.Description === 'Vắng mặt'
-								? 'error'
-								: 'default'
-					}
-				>
-					{props.Description}
-				</Tag>
-			</div>
-		</div>
-	);
+		);
+	};
 
 	return (
 		<div>
@@ -244,20 +249,38 @@ const SetupSchedules: React.FC = () => {
 					</Typography.Title>
 				</Col>
 			</Row>
-			<div style={{ display: 'flex', marginTop: '20px' }}>
-				<DropdownCenter
-					centers={centers}
-					selectedCenter={selectedCenter || 'all'}
-					onChange={handleCenterChange}
-				/>
-				{selectedCenter !== 'all' && (
-					<DropdownCourse
-						courses={courses}
-						selectedCourse={selectedCourse || 'all'}
-						onChange={handleCourseChange}
-					/>
-				)}
-			</div>
+
+			<Row>
+				<Col span={20}>
+					<div style={{ display: 'flex', marginTop: '20px' }}>
+						<DropdownCenter
+							centers={centers}
+							selectedCenter={selectedCenter || 'all'}
+							onChange={handleCenterChange}
+						/>
+						{selectedCenter !== 'all' && (
+							<DropdownCourse
+								courses={courses}
+								selectedCourse={selectedCourse || 'all'}
+								onChange={handleCourseChange}
+							/>
+						)}
+					</div>
+				</Col>
+				<Col span={4}>
+					<div style={{ marginTop: '20px' }}>
+						<Button
+							type="default"
+							icon={<SyncOutlined />}
+							onClick={handleRefresh}
+							style={{ float: 'right' }}
+						>
+							Làm mới
+						</Button>
+					</div>
+				</Col>
+			</Row>
+
 			<ScheduleComponent
 				ref={scheduleRef}
 				height="650px"
