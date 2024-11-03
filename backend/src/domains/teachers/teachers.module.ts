0f2032d13e0ff32387c8b5ada06de0d9a9d01dcf
Quốc Chương <quocchuong2609@gmail.com>
2024-11-03 10:39:25 +0700

    update: release backend v1.3.8

--- backend/src/domains/teachers/teachers.module.ts
+++ backend/src/domains/teachers/teachers.module.ts
@@ -3,11 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { TeachersController } from '@/domains/teachers/teachers.controller';
 import { TeachersService } from '@/domains/teachers/teachers.service';
+import { CenterSchema } from '@/schemas/center.schema';
 import { TeacherSchema } from '@/schemas/teacher.schema';
 
 @Module({
 	imports: [
-		MongooseModule.forFeature([{ name: 'Teacher', schema: TeacherSchema }]),
+		MongooseModule.forFeature([
+			{ name: 'Teacher', schema: TeacherSchema },
+			{ name: 'Center', schema: CenterSchema },
+		]),
 	],
 	controllers: [TeachersController],
 	providers: [TeachersService],
